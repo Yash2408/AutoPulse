a1b8db48aef45a38c443fee545b5e0856e952523
Yash Sarvaiya <93860285+Yash2408@users.noreply.github.com>
2023-07-31 23:29:50 +0530

    Update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,15 +7,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAS0uNHoiA1oI1xjGW2tMZI-Hu2wL4AUC0",
-    authDomain: "licdataapp.firebaseapp.com",
-    projectId: "licdataapp",
-    storageBucket: "licdataapp.appspot.com",
-    messagingSenderId: "94908361949",
-    appId: "1:94908361949:web:3f3fd82bad071564c3bf34",
-    measurementId: "G-35ZC3432XD"
-};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
